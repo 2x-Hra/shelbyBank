6d85e4a280b254e684039e16afa9eaef4f86565d
Hosseinreza <hrakbari77@gmail.com>
2023-04-17 07:05:02 +0330

    Add disable in transaction

--- app/src/app_frontend/src/index.js
+++ app/src/app_frontend/src/index.js
@@ -4,7 +4,6 @@ window.addEventListener("load", async function(){
   // console.log('hii');
 
   const currentAmount = await app_backend.checkBalance();
-  
   this.document.getElementById("value").innerText = Math.round(currentAmount*100) / 100;
 });
 
@@ -12,9 +11,18 @@ document.querySelector("form").addEventListener("submit", async function(event){
   event.preventDefault();
   console.log("submited");
 
+  const button = event.target.querySelector("#submit-btn");
+
   const inputAmount = parseFloat(document.getElementById("input-amount").value);
   const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
 
+  button.setAttribute("disabled", true);
+
   await app_backend.topUp(inputAmount);
      
+  const currentAmount = await app_backend.checkBalance();
+  this.document.getElementById("value").innerText = Math.round(currentAmount*100) / 100;
+
+
+  button.removeAttribute("disabled");
 });
