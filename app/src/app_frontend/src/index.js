9f45f992035d2e45331c87eb8698111f26524176
Hosseinreza <hrakbari77@gmail.com>
2023-04-20 06:37:23 +0330

    Add currentAmount

--- app/src/app_frontend/src/index.js
+++ app/src/app_frontend/src/index.js
@@ -1,5 +1,8 @@
 import { app_backend } from "../../declarations/app_backend";
 
 window.addEventListener("load", function(){
-  console.log('hii');
+  // console.log('hii');
+
+  const currentAmount = app_backend.checkBalance();
+  this.document.getElementById("value").innerText = currentAmount;
 });
